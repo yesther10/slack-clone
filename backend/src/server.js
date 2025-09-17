6ea139853faab32471e2913c0bf96e56973991a9
Yesther Guerra <yguerra@martech.cl>
2025-09-17 13:06:05 -0300

    DB connection and environment variables

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import expres from "express";
 import { ENV } from "../config/env.js";
+import { connectDB } from "../config/db.js";
 
 const app = expres();
 
@@ -9,4 +10,5 @@ app.get("/", (req, res) => {
 
 app.listen(ENV.PORT, () => {
   console.log(`Server is running on port ${ENV.PORT}`);
+  connectDB();
 });
